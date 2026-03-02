189d78c1ac7d91136a6cc1fc7f53937b33d70124
Piyula <annepiyula@gmail.com>
2026-03-02 13:28:22 +0530

    some chanegs

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 
 
 
-const express = require("express");
-const app = express();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const express = require("express"); // Express framework for building the server
+const app = express(); // Create an Express application 
+const http = require("http").Server(app); // Create on HTTP server usin the Express app
+const io = require("socket.io")(http); // Integrate Socket.IO for real-time communication
 
 // DB
-const { connectDB } = require("./db/mongo");
+const { connectDB } = require("./db/mongo"); // Import the function to connect to MongoDB
 
 // Connect to MongoDB ONE TIME
-connectDB();
+connectDB(); 
 
 app.use(express.json());
 app.use(express.static("public"));
